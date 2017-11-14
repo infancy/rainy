ad8a21799bb47af2a62e8ec3dc4a955f37ed94ca
Infancy <326948850@qq.com>
2017-11-14 13:19:02 +0800

    调整代码结构

--- src/core/api.h
+++ src/core/api.h
@@ -6,34 +6,37 @@
 #ifndef RAINY_CORE_API_H
 #define RAINY_CORE_API_H
 
-#include"camera.h"
-#include"film.h"
-#include"filter.h"
-#include"integrator.h"
-#include"light.h"
-#include"primitive.h"
-#include"scene.h"
-#include"texture.h"
+#include "camera.h"
+#include "film.h"
+#include "filter.h"
+#include "integrator.h"
+#include "light.h"
+#include "primitive.h"
+#include "scene.h"
+#include "texture.h"
 
-#include"camera/perspective.h"
-//#include"integrator/bdpt.h"
-#include"integrator/eyelight.h"
-#include"integrator/pathtracing.h"
-#include"integrator/directlight.h"
-#include"integrator/sppm.h"
-//#include"integrator/test.h"
-#include"light/diffuse.h"
-#include"light/distance.h"
-#include"light/point.h"
-#include"material/glass.h"
-#include"material/matte.h"
-#include"material/mirror.h"
-#include"sampler/random.h"
-#include"sampler/uniform.h"
-#include"shape/sphere.h"
-#include"shape/rectangle.h"
+#include "camera/perspective.h"
 
-using namespace std;
+// #include "integrator/bdpt.h"
+#include "integrator/eyelight.h"
+#include "integrator/pathtracing.h"
+#include "integrator/directlight.h"
+#include "integrator/sppm.h"
+// #include "integrator/test.h"
+
+#include "light/diffuse.h"
+#include "light/distance.h"
+#include "light/point.h"
+
+#include "material/glass.h"
+#include "material/matte.h"
+#include "material/mirror.h"
+
+#include "sampler/random.h"
+#include "sampler/uniform.h"
+
+#include "shape/sphere.h"
+#include "shape/rectangle.h"
 
 namespace rainy
 {
@@ -45,12 +48,11 @@ enum class Intetrator_type
 
 void rainy_render();
 
-
-shared_ptr<Scene> rainy_create_scene();
+std::shared_ptr<Scene> rainy_create_scene();
 
 Integrator* rainy_create_integrator(const Scene& scene);
 
-}	//namespace rainy
+}	// namespace rainy
 
 
 #endif //RAINY_CORE_API_H
